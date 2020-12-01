36b0559c5bad8fd6e386217d1fbd68590a07697b
RossSingleton <ross-singleton@hotmail.co.uk>
2020-12-01 09:36:58 +0000

    updating to work for pt 2

--- puzzle1/puzzle1.py
+++ puzzle1/puzzle1.py
@@ -3,11 +3,12 @@ def main():
     array_length = len(f)
     for x in range(array_length):
         for y in range(array_length):
-            result = checkSum(f[x], f[y])
-            if result == int(2020):
-                print(result)
-                finalResult = int(f[x]) * int(f[y])
-                return finalResult
+            for j in range(array_length):
+                result = checkSum(f[x], f[y], f[j])
+                if result == int(2020):
+                    print(result)
+                    finalResult = int(f[x]) * int(f[y]) * int(f[j])
+                    return finalResult
 
 def readFile(fileName):
     fileObj = open(fileName, "r") #opens the file in read mode
@@ -15,8 +16,8 @@ def readFile(fileName):
     fileObj.close()
     return words
 
-def checkSum(num1, num2):
-    result = int(num1) + int(num2)
+def checkSum(num1, num2, num3):
+    result = int(num1) + int(num2) + int(num3)
     return result
 
 print(main())
